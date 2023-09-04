368935baa21857f98c6c7b9a5b0a90c97c41da57
baophan-dot <131897142+baophan-dot@users.noreply.github.com>
2023-09-04 22:19:00 +0700

    Update history.js

--- history.js
+++ history.js
@@ -26,32 +26,5 @@ function clearHistory() {
     // Refresh the page to reflect the cleared history
     location.reload();
 }
-
-// Function to set a cookie with a specified name, value, and expiration (in days)
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-// Function to get the value of a cookie by its name
-function getCookie(name) {
-    const cookieName = name + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookie.split(';');
-
-    for (let i = 0; i < cookieArray.length; i++) {
-        let cookie = cookieArray[i];
-        while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1);
-        }
-        if (cookie.indexOf(cookieName) == 0) {
-            return cookie.substring(cookieName.length, cookie.length);
-        }
-    }
-    return "";
-}
-
 // Call populateNumberHistory when the history page loads
 populateNumberHistory();
