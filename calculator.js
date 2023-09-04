6f8ad1e1e7666ebe149cbb1440b4fdefc63c4be5
baophan-dot <131897142+baophan-dot@users.noreply.github.com>
2023-09-04 22:19:19 +0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -21,28 +21,3 @@ function saveNumberToHistory(number) {
     setCookie("numberHistory", JSON.stringify(numberHistory), 365);
 }
 
-// Function to set a cookie with a specified name, value, and expiration (in days)
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-// Function to get the value of a cookie by its name
-function getCookie(name) {
-    const cookieName = name + "=";
-    const decodedCookie = decodeURIComponent(document.cookie);
-    const cookieArray = decodedCookie.split(';');
-
-    for (let i = 0; i < cookieArray.length; i++) {
-        let cookie = cookieArray[i];
-        while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1);
-        }
-        if (cookie.indexOf(cookieName) == 0) {
-            return cookie.substring(cookieName.length, cookie.length);
-        }
-    }
-    return "";
-}
